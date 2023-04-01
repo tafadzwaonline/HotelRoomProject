587bf2a60ff1ea75b530f04adc4c844d1b6bdb6d
Tafadzwa Kahwai <kahwaitafadzwa@gmail.com>
2023-04-01 14:15:41 +0200

    Changes

---
+++ HiddenVilla_Server/wwwroot/js/common.js
@@ -0,0 +1,12 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type == "success")
+    {
+        // Display a success toast, with a title
+        toastr.success(message, 'Operation Successful')
+    }
+    if (type == "error")
+    {
+        // Display an error toast, with a title
+        toastr.error(message, 'Operation Failed')
+    }
+}
